87011fb09899d3735fe61b4528c54ef3d4daa3f9
Michael Graff <explorer@flame.org>
2024-03-26 05:07:08 -0500

    feat: add Signer.SetSigningKey()

--- ssdjwtauth/signer.go
+++ ssdjwtauth/signer.go
@@ -3,12 +3,14 @@ package ssdjwtauth
 import (
 	"crypto"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
 type Signer struct {
+	sync.Mutex
 	KeyID string
 	Key   crypto.PrivateKey
 }
@@ -38,8 +40,23 @@ func (s *Signer) MakeClaims(now time.Time, expiry time.Time, id string, ssd SSDC
 	}
 }
 
+func (s *Signer) SetSigningKey(keyID string, pemkey []byte) error {
+	rk, err := jwt.ParseRSAPrivateKeyFromPEM(pemkey)
+	if err != nil {
+		return fmt.Errorf("unable to parse private key PEM for keyid %s: %v", keyID, err)
+	}
+	s.Lock()
+	defer s.Unlock()
+	s.KeyID = keyID
+	s.Key = rk
+	return nil
+}
+
 func (s *Signer) SignToken(claims SsdJwtClaims) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodPS256, claims)
+
+	s.Lock()
+	defer s.Unlock()
 	token.Header["kid"] = s.KeyID
 	return token.SignedString(s.Key)
 }
